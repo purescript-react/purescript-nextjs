108fbcd0e45498c3ccdd07c0380420a269d85ca4
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2023-01-06 15:06:11 +0000

    Add Router.replace

--- spago.dhall
+++ spago.dhall
@@ -12,10 +12,12 @@ to generate this file without the comments in this block.
 -}
 { name = "my-project"
 , dependencies =
-  [ "aff-promise"
+  [ "aff"
+  , "aff-promise"
   , "console"
   , "effect"
   , "functions"
+  , "maybe"
   , "nullable"
   , "prelude"
   , "react-basic"
--- src/Next/Router.js
+++ src/Next/Router.js
@@ -1,24 +1,27 @@
-export { useRouter as useRouter_ } from 'next/router'
+export { useRouter as useRouter_ } from "next/router";
 import router from "next/router";
 
 export function _on(event) {
-  return function(cb) {
-    return function() {
+  return function (cb) {
+    return function () {
       router.events.on(event, cb);
     };
   };
 }
 
 export function _off(event) {
-  return function(cb) {
-    return function() {
+  return function (cb) {
+    return function () {
       router.events.off(event, cb);
     };
   };
 }
 
-export const query = (router) => router.query
+export const query = (router) => router.query;
 
-export const push = (router) => (route) => () => router.push(route)
+export const push = (router) => (route) => () => router.push(route);
 
-export const route = (router) => router.route
+export const route = (router) => router.route;
+
+export const replaceImpl = (router) => (route) => (as) => (options) => () =>
+  router.replace(route, as, options);
